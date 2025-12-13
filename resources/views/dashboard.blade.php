e6811d52b735990f278515a98c2a42ef5746ecc4
khemsingh <khemsinhnishad3035@gmail.com>
2025-12-13 15:15:44 +0530

    upt

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -52,7 +52,7 @@
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
-    <script>
+    <script> 
         $('#clockInButton').click(function() {
             let btn = $(this);
             btn.prop('disabled', true);
